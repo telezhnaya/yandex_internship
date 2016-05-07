96d250f8aa73a6c889ada19eb557a5fd0471ba34
telezhnaya <olyatelezhnaya@gmail.com>
2016-05-07 10:17:53 +0300

    finish

--- linguistics/Pedigree.py
+++ linguistics/Pedigree.py
@@ -3,6 +3,7 @@ import pytest
 
 from enum import Enum
 
+
 # Из приведенных тестов я сделала вывод, что братья и сестры бывают
 # только родными в пределах задания.
 # Если у жены есть ребенок, то и у мужа он тоже есть.
@@ -41,39 +42,73 @@ class Person(object):
     def __hash__(self):
         return self.name.__hash__()
 
-    def marry(self, wife):
-        self.spouse, wife.spouse = wife, self
-        self.set_opposite_gender(wife)
-        wife.set_opposite_gender(self)
+    def add_link(self, other, rel_type):
+        if rel_type in ["child", "son", "daughter"]:
+            self.update_parents(other, other.spouse)
+            other.children.add(self)
+        elif rel_type in ["parent", "father", "mother"]:
+            self.children.add(other)
+            other.update_parents(self, self.spouse)
+        elif rel_type in ["spouse", "husband", "wife"]:
+            self.marry(other)
+        elif rel_type in ["sibling", "brother", "sister"]:
+            if self.dad.real:
+                other.update_parents(self.dad, self.mom)
+            # Теперь other имеет правильных родителей.
+            # Останемся либо с реальными родителями, либо с одним вымышленным
+            self.update_parents(other.dad, other.mom)
+        else:
+            raise Exception("Unknown relation type: " + rel_type)
+
+    def parent_request(self, gender):
+        return ", ".join(self.get_parents(gender))
+
+    def child_request(self, gender):
+        return ", ".join(self.get_children(gender))
+
+    def sibling_request(self, gender):
+        siblings = self.dad.get_children(gender)
+        siblings -= {self.name, self.name + "?"}
+        return ", ".join(siblings)
+
+    def spouse_request(self, gender):
+        if self.spouse:
+            ending = "" if self.spouse.gender == gender else "?"
+            return self.spouse.name + ending
+
+    def update_gender(self, gender):
+        self._set_gender(gender)
+        if self.spouse:
+            self.spouse.set_opposite_gender(self)
 
     def update_parents(self, new_dad, new_mom=None):
         if self.dad:
             if not self.dad.real:
+                # Тогда избавимся от него и возьмем новых родителей
                 new_dad.children.update(self.dad.children)
                 for child in self.dad.children:
                     child.dad, child.mom = new_dad, new_mom
             elif self.dad != new_dad:
                 assert not new_mom or new_mom == self.dad
                 self.mom = new_dad
-
-                for child in self.dad.children:
-                    child.dad, child.mom = self.dad, self.mom
-                for child in self.mom.children:
-                    child.dad, child.mom = self.dad, self.mom
         else:
             self.dad, self.mom = new_dad, new_mom
         if self.dad and self.mom:
             self.dad.marry(self.mom)
 
+    def marry(self, wife):
+        self.spouse, wife.spouse = wife, self
+        self.set_opposite_gender(wife)
+        wife.set_opposite_gender(self)
+        self.set_parent_to_children(wife)
+        wife.set_parent_to_children(self)
+        self.children.update(wife.children)
+        wife.children = self.children
+
     def _set_gender(self, gender):
         if gender != Gender.unknown:
             assert self.gender in {gender, Gender.unknown}
             self.gender = gender
-
-    def update_gender(self, gender):
-        self._set_gender(gender)
-        if self.spouse:
-            self.spouse.set_opposite_gender(self)
 
     def set_opposite_gender(self, other):
         if other.gender == Gender.male:
@@ -85,65 +120,24 @@ class Person(object):
         for child in self.children:
             child.dad, child.mom = self, parent
 
-    def add_link(self, other, rel_type):
-        if rel_type in ["child", "son", "daughter"]:
-            self.update_parents(other, other.spouse)
-            other.children.add(self)
-        elif rel_type in ["parent", "father", "mother"]:
-            self.children.add(other)
-            other.update_parents(self, self.spouse)
-        elif rel_type in ["husband", "wife"]:
-            self.spouse, other.spouse = other, self
-            self.set_parent_to_children(other)
-            other.set_parent_to_children(self)
-        elif rel_type in ["brother", "sister"]:
-            if self.dad.real:
-                other.update_parents(self.dad, self.mom)
-            # Останемся либо с реальными родителями, либо с одним вымышленным
-            self.update_parents(other.dad, other.mom)
-        else:
-            raise Exception("Unknown relation type: " + rel_type)
-
     def get_name_for_request(self, gender):
         if gender in (self.gender, Gender.unknown):
             return self.name
         if self.gender == Gender.unknown:
             return self.name + "?"
 
-    def parent_request_helper(self, gender=Gender.unknown):
+    def get_parents(self, gender=Gender.unknown):
         answer = []
         # Ну, женщины и мужчины часто меняются ролями. :-D
         if self.dad.real:
             answer.append(self.dad.get_name_for_request(gender))
             if self.mom:
                 answer.append(self.mom.get_name_for_request(gender))
-        return [name for name in answer if name]
+        return {name for name in answer if name}
 
-    def parent_request(self, gender):
-        return ", ".join(self.parent_request_helper(gender))
-
-    def get_children(self, gender):
+    def get_children(self, gender=Gender.unknown):
         children = (ch.get_name_for_request(gender) for ch in self.children)
         return {ch for ch in children if ch}
-
-    def child_request_helper(self, gender=Gender.unknown):
-        children = self.spouse.get_children(gender) if self.spouse else set()
-        return children | self.get_children(gender)
-
-    def child_request(self, gender):
-        return ", ".join(self.child_request_helper(gender))
-
-    def sibling_request(self, gender):
-        siblings = self.dad.get_children(gender)
-        if self.mom:
-            siblings.update(self.mom.get_children(gender))
-        siblings -= {self.name, self.name + "?"}
-        return ", ".join(siblings)
-
-    def spouse_request(self, gender):
-        if self.spouse:
-            ending = "" if self.spouse.gender == gender else "?"
-            return self.spouse.name + ending
 
 
 class PedigreeHolder:
@@ -195,15 +189,15 @@ class PedigreeHolder:
             return person.sibling_request(gender) or self.DEFAULT_ANSWER
         if rel_type in ["grandchild", "grandson", "granddaughter"]:
             ans = set()
-            children = person.child_request_helper()
+            children = person.get_children()
             for child in children:
-                ans.update(self.people[child].child_request_helper(gender))
+                ans.update(self.people[child].get_children(gender))
             return ", ".join(ans) or self.DEFAULT_ANSWER
         if rel_type in ["grandfather", "grandmother"]:
             ans = set()
-            parents = person.parent_request_helper()
+            parents = person.get_parents()
             for parent in parents:
-                ans.update(self.people[parent].parent_request_helper(gender))
+                ans.update(self.people[parent].get_parents(gender))
             return ", ".join(ans) or self.DEFAULT_ANSWER
 
     def gender_request(self, name, gender_word):
@@ -232,7 +226,8 @@ class PedigreeHolder:
 
 ph = PedigreeHolder()
 ph.add("Carol is Ann's daughter")
-ph.add("Ann is Brett's wife")
+# ph.add("Ann is Brett's wife")
+ph.add("Carol is Darren's sister")
 ph.add("Darren is Brett's son")
 ph.add("Brett is Darren's father")
 ph.add("Carol is Frank's sister")
@@ -241,32 +236,6 @@ ph.add("Nathan is Emily's brother")
 ph.add("John is Emily's brother")
 ph.add("Filmore is Jeremy's son")
 ph.add("Filmore is Lily's son")
-
-aph = PedigreeHolder()
-aph.add("Ann is Rosa's mother")
-aph.add("Lily is John's daughter")
-aph.add("Jeremy is John's son")
-aph.add("Mary is Jeremy's sister")
-aph.add("Lily is Jeremy's sister")
-aph.add("Rosa is Jeremy's sister")
-
-print(aph.request("Who is Mary's mother?"))
-assert aph.request("Who is Mary's mother?") == "Ann"
-assert aph.request("Who is Ann's husband?") == "John"
-assert aph.request("Who is Mary's brother?") == "Jeremy"
-
-aph2 = PedigreeHolder()
-aph2.add("Ann is Rosa's mother")
-aph2.add("Ann is Lily's mother")
-aph2.add("Lily is John's daughter")
-
-assert aph2.request("Who is Lily's mother?") == "Ann"
-assert aph2.request("Who is Lily's father?") == "John"
-assert aph2.request("Who is Rosa's mother?") == "Ann"
-assert aph2.request("Who is Rosa's father?") == "John"
-assert aph2.request("Who is Ann's husband?") == "John"
-assert aph2.request("Who is Rosa's sister?") == "Lily"
-
 
 
 @pytest.mark.parametrize("question, answer", [
--- vertical_services/Solution.py
+++ vertical_services/Solution.py
@@ -1,18 +1,13 @@
 from collections import defaultdict
 
 
-class KeyDict(defaultdict):
-    def __missing__(self, key):
-        ret = self[key] = self.default_factory(key)
-        return ret
-
-
-with open('unix.mailbox', mode='rb') as file:
-    files = KeyDict(lambda x: open(x + '.mailbox', 'wb'))
+with open('unix.mailbox', 'rb') as file:
     author = None
+    files = defaultdict(lambda: open('{}.mailbox'.format(author), 'wb'))
     for line in file:
         if line.startswith(b'From '):
-            author = line.split(b' ', 2)[1].decode('utf-8')
+            _, author, *_ = line.split(b' ', 2)
+            author = author.decode('utf-8')
         files[author].write(line)
 
     for f in files.values():
